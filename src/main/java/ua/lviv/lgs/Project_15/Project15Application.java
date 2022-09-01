6ce8e557ec3bced8f00143ad7f425a3810ca87fd
RomanSukhai <romansukhai@gmail.com>
2022-09-01 15:02:39 +0300

    add

---
+++ src/main/java/ua/lviv/lgs/Project_15/Project15Application.java
@@ -0,0 +1,13 @@
+package ua.lviv.lgs.Project_15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project15Application.class, args);
+	}
+
+}
---
+++ src/test/java/ua/lviv/lgs/Project_15/Project15ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.lgs.Project_15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
